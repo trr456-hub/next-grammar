59c0aaca7fe8b855f012b7efd80adc533a12c620
gorhf9397 <trr456@naver.com>
2023-12-13 18:23:45 +0900

    style 생성 API fetch 연결 기본view뿌려줌 데이터연결

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,10 @@
-import NavBar from "../../components/NavBar";
+import Layout from "../../components/Layout";
+import "../../styles/globals.css";
 
 export default function App({ Component, PageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...PageProps} />
-      <style jsx global>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
-    </>
+    </Layout>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,10 @@
+import Head from "next/head";
+import Seo from "../../components/Seo";
+
 export default function Tomato() {
   return (
     <div>
+      <Seo title="About" />
       <h1>About</h1>
     </div>
   );

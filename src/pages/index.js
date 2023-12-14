4f13ad992c1cca123e3ebdf1815000509c57727c
gorhf9397 <trr456@naver.com>
2023-12-14 19:38:04 +0900

    동적 url 생성 url을 통한 상세페이지 생성 url 마스킹 처리

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,46 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../../components/Seo";
 
 export default function Home({ results }) {
-  console.log("results 데이터를 잘 받아오고 있는지 확인 : ", results);
+  //   console.log("results 데이터를 잘 받아오고 있는지 확인 : ", results);
+  const router = useRouter();
+  const clk = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
 
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => clk(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              {movie.original_title}
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -28,6 +58,7 @@ export default function Home({ results }) {
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          cursor: pointer;
         }
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);

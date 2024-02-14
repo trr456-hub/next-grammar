dc8e9f9996d66a67742876cb2faaa9021fefc311
seokgeun <trr456@naver.com>
2024-02-14 14:48:47 +0900

    NEXT.js 14버전으로 재생성

--- components/Seo.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function Seo({ title }) {
-  return (
-    <Head>
-      <title>{title} | Next movies</title>
-    </Head>
-  );
-}
--- src/pages/404.js
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return "What are u doing here?";
-}
